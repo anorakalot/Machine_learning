b488f7fb868c7b1d1d654ab047b866c5cc4ef1d9
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-04-09 22:35:08 +0000

    Add files via upload

---
+++ pandas_1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#series are a single column
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
+
+
+cities = pd.DataFrame({"City name":city_names, "Population":population})
+
+
+#print(cities.describe())
+print(cities.head())
+print()
+
+#if reading from a csv
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe.describe()
+
+print(california_housing_dataframe.head())
+#scalifornia_housing_dataframe.hist("housing_median_age")
